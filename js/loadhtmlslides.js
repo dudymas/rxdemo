57e0ad2fb624f374a8cbc98c153d25b12dd589a7
Jeremy White <dudymas@gmail.com>
2014-07-05 00:53:43 -0400

    fixing styles and adding codemirror

--- js/loadhtmlslides.js
+++ js/loadhtmlslides.js
@@ -4,6 +4,23 @@
     /*globals alert*/
     'use strict';
 
+    var codeMirror = window.CodeMirror;
+    var console = window.console;
+    var handleCode = window.eval;
+
+    var setupCodeMirror = function setupCodeMirror() {
+        var keymap = {};
+        keymap['Ctrl-Enter'] = function (cm) { handleCode(cm.getDoc().getValue()); };
+        var opts = {mode: 'javascript', theme: 'lesser-dark', extraKeys: keymap};
+        var myTextArea = document.querySelector('textarea.CodeMirror');
+        if (myTextArea) {
+            opts.value = myTextArea.value;
+            codeMirror(function (elt) {
+                myTextArea.parentNode.replaceChild(elt, myTextArea);
+            }, opts);
+        }
+    };
+
     var querySlidingHtml = function () {
         var sections = document.querySelectorAll('[data-html]'),
             section, j, jlen;
@@ -19,6 +36,7 @@
                         if (xhr.readyState === 4) {
                             if (xhr.status >= 200 && xhr.status < 300) {
                                 section.innerHTML = xhr.responseText;
+                                setupCodeMirror();
                             } else {
                                 section.outerHTML = '<section data-state="alert">ERROR: The attempt to fetch ' + url + ' failed with the HTTP status ' + xhr.status + '. Check your browser\'s JavaScript console for more details.</p></section>';
                             }
